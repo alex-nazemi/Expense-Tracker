68d345ce6ee2ff139e0ea464075df94a7c13f744
alex-nazemi <alexandernazemi@gmail.com>
2023-07-29 20:18:35 +0100

    created shared inputChangeHandler function for multiple states for same component function

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,43 +2,44 @@ import "./ExpenseForm.css";
 import { useState } from "react";
 
 export default function ExpenseForm() {
-  //   const [enteredTitle, setEnteredTitle] = useState("");
-  //   const [enteredAmount, setEnteredAmount] = useState("");
-  //   const [enteredDate, setEnteredDate] = useState("");
+    const [enteredTitle, setEnteredTitle] = useState("");
+    const [enteredAmount, setEnteredAmount] = useState("");
+    const [enteredDate, setEnteredDate] = useState("");
 
-  const [userInput, setUserInput] = useState({
-    enteredTitle: "",
-    enteredAmount: "",
-    enteredDate: "",
-  });
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: "",
+  // });
 
-  const titleChangeHandler = (event) => {
-    setUserInput((prevState) => ({
-      ...prevState,
-      enteredTitle: event.target.value,
-    }));
-  };
+  const inputChangeHandler = (identifier, value) => {
+    if (identifier === "title") {
+      setEnteredTitle(value); 
+    } else if (identifier === "amount") {
+      setEnteredAmount(value);
+    } else {
+      setEnteredDate(value);
+    }
+  }
 
-  const amountChangeHandler = (event) => {
-    setUserInput((prevState) => ({
-      ...prevState,
-      enteredAmount: event.target.value,
-    }));
-  };
+  // const titleChangeHandler = (event) => {
+  //   setEnteredTitle(event.target.value); 
+  // };
 
-  const dateChangeHandler = (event) => {
-    setUserInput((prevState) => ({
-      ...prevState,
-      enteredDate: event.target.value,
-    }));
-  };
+  // const amountChangeHandler = (event) => {
+  //   setEnteredAmount(event.target.value); 
+  // };
+
+  // const dateChangeHandler = (event) => {
+  //   setEnteredDate(event.target.value); 
+  // };
 
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title:</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input type="text" onChange={event => inputChangeHandler("title", event.target.value)} />
         </div>
         <div className="new-expense__control">
           <label>Amount:</label>
@@ -46,7 +47,7 @@ export default function ExpenseForm() {
             type="number"
             min="0.01"
             step="0.01"
-            onChange={amountChangeHandler}
+            onChange={event => inputChangeHandler("amount", event.target.value)}
           />
         </div>
         <div className="new-expense__control">
@@ -55,7 +56,7 @@ export default function ExpenseForm() {
             type="date"
             min="2020-01-01"
             max="2023-12-31"
-            onChange={dateChangeHandler}
+            onChange={event => inputChangeHandler("date", event.target.value)}
           />
         </div>
       </div>
