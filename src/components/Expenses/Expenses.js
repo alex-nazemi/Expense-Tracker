9254cd40ec11c125f433e2af02d42343a688502f
alex-nazemi <alexandernazemi@gmail.com>
2023-07-31 23:31:23 +0100

    created expenses filter component and css file, update state of expenses component with selected filter year using prop function and introduced two way binding to reset default value of drop down

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,18 +1,34 @@
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 import "./Expenses.css";
 import Card from "../UI/Card";
 
 export default function Expenses(props) {
+  const [expenseYear, setExpenseYear] = useState("2020");
+
+  const filterHandler = (year) => {
+    setExpenseYear(year);
+  };
+
+  // useEffect(() => {
+  //   console.log("From expenses.js:");
+  //   console.log(expenseYear);
+  // }, [expenseYear]);
+
   return (
-    <Card className="expenses">
-      {props.expenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          name={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter handler={filterHandler} selectedYear={expenseYear}/>
+        {props.expenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            name={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
   );
 }
