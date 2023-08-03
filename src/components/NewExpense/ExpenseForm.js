7f349777e09b00d033f694e5606045bf11a48731
alex-nazemi <alexandernazemi@gmail.com>
2023-08-03 11:28:40 +0100

    added cancel button to expense form which collapses form, added new expense button which opens form for data to be submitted, on submission form collapases again

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,59 +2,70 @@ import "./ExpenseForm.css";
 import { useState } from "react";
 
 export default function ExpenseForm({ onSaveExpenseData }) {
-    const [enteredTitle, setEnteredTitle] = useState("");
-    const [enteredAmount, setEnteredAmount] = useState("");
-    const [enteredDate, setEnteredDate] = useState("");
-
-  // const [userInput, setUserInput] = useState({
-  //   enteredTitle: "",
-  //   enteredAmount: "",
-  //   enteredDate: "",
-  // });
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  const [showExpenseForm, setShowExpenseForm] = useState(false);
 
   const inputChangeHandler = (identifier, value) => {
     if (identifier === "title") {
-      setEnteredTitle(value); 
+      setEnteredTitle(value);
     } else if (identifier === "amount") {
       setEnteredAmount(value);
     } else {
       setEnteredDate(value);
     }
-  }
+  };
 
   const submitHandler = (event) => {
     event.preventDefault(); // prevents page from refreshing on form submission
     const expenseData = {
       title: enteredTitle,
       amount: enteredAmount,
-      date: new Date(enteredDate) // parses date string and converts it into date object
+      date: new Date(enteredDate), // parses date string and converts it into date object
     };
 
     onSaveExpenseData(expenseData); // passes object containing states into parent function, passing data upwards
     setEnteredTitle(""); // overrides what the user entered after the form was submitted and clears the input
     setEnteredAmount("");
-    setEnteredDate(""); 
-    
+    setEnteredDate("");
+    cancelHandler();
+  };
+
+  const cancelHandler = () => {
+    setShowExpenseForm(false);
   }
 
-  // const titleChangeHandler = (event) => {
-  //   setEnteredTitle(event.target.value); 
-  // };
+  const addNewExpenseHandler = () => {
+    setShowExpenseForm(true);
+  }
 
-  // const amountChangeHandler = (event) => {
-  //   setEnteredAmount(event.target.value); 
-  // };
+  if (!showExpenseForm) {
+    return (
+      <div>
+        <button onClick={addNewExpenseHandler}>Add New Expense</button>
+      </div>
+    )
+  }
 
-  // const dateChangeHandler = (event) => {
-  //   setEnteredDate(event.target.value); 
-  // };
+
+
+  // add "add new expense button"
+  // collapose form, only show new expense button
+  //
 
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title:</label>
-          <input type="text" value={enteredTitle} onChange={event => inputChangeHandler("title", event.target.value)} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={(event) =>
+              inputChangeHandler("title", event.target.value)
+            }
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount:</label>
@@ -63,7 +74,9 @@ export default function ExpenseForm({ onSaveExpenseData }) {
             min="0.01"
             step="0.01"
             value={enteredAmount}
-            onChange={event => inputChangeHandler("amount", event.target.value)}
+            onChange={(event) =>
+              inputChangeHandler("amount", event.target.value)
+            }
           />
         </div>
         <div className="new-expense__control">
@@ -73,11 +86,12 @@ export default function ExpenseForm({ onSaveExpenseData }) {
             min="2020-01-01"
             max="2023-12-31"
             value={enteredDate}
-            onChange={event => inputChangeHandler("date", event.target.value)}
+            onChange={(event) => inputChangeHandler("date", event.target.value)}
           />
         </div>
       </div>
       <div className="new-expense__actions">
+        <button onClick={cancelHandler}>Cancel</button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
